da186fab1d58d9ea10e23de2b3906fdad647141f
bryantanjw <bryantanjw01@gmail.com>
2023-09-10 14:36:08 +0800

    Testing stripe webhook in prod

--- app/pricing/components/pricing.tsx
+++ app/pricing/components/pricing.tsx
@@ -201,7 +201,7 @@ export default function Pricing({
             )}
           </div>
         </div>
-        <div className="mt-12 space-y-4 sm:mt-16 sm:space-y-0 sm:grid sm:grid-cols-2 sm:gap-6 lg:max-w-4xl lg:mx-auto xl:max-w-none xl:mx-0 xl:grid-cols-3">
+        <div className="mt-12 space-y-4 sm:mt-16 sm:space-y-0 sm:grid sm:grid-cols-2 sm:gap-6 lg:max-w-4xl lg:mx-auto xl:max-w-none xl:mx-0 lg:grid-cols-3">
           {products.map((product) => {
             const price = product?.prices?.find(
               (price) => price.interval === billingInterval
