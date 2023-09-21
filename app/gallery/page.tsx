c6d8877be70494ae4d72057f3375fd8ec15d2797
bryantanjw <bryantanjw01@gmail.com>
2023-09-21 20:41:57 +0800

    Fixed gallery footer

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -4,7 +4,7 @@ import { getAll } from "@/sanity/sanity-utils";
 export default async function GalleryPage() {
   const data = await getAll();
   return (
-    <div className="items-center justify-center h-screen">
+    <div className="items-center justify-center">
       <ImageGrid data={data} />
     </div>
   );
